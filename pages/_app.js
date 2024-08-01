227c2fe5e77bc1faff1c49460f63466f56fb96f3
xtom <timmy000728@gmail.com>
2024-08-01 13:08:19 +1000

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import Footer from "@/componets/Home/Footer";
+import Header from "@/componets/Home/Header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+  )
+
 }
